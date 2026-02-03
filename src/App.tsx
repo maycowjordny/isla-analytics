f28e1081955c8fa530525542cbce7e75b3bf5c8f
maycow jordny <maycowjordny2015@gmail.com>
2026-02-03 16:47:42 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button size="lg">Hello World</Button>
+    </div>
+  );
+}
+
+export default App;
